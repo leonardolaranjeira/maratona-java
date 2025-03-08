ba8c2f3f8dc599c4c9db33f951c278792e26d148
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-03-08 20:36:36 -0300

    Tipos primitivos e alguns conceitos
    Estudo de casting, tipos primitivos, Tabela ASCII e String.

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaToranjo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaToranjo.java
@@ -1,9 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
 /**
- * Isso é um comentário <b> javadoc </b>
+ * Isso é um comentário <b>javadoc</b>
  */
-
 public class Aula01OlaToranjo {
-    public static void main(String[] args) {System.out.println("AMATERASU");}
+    // Isso é um comentário de uma linha
+    /*
+ Isso é um comentário de múltiplas linhas
+ */
+    public static void main(String[] args) {
+        System.out.println("AMATERASU");
+    }
 }
--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,7 +2,30 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        int Age = 10;
-        System.out.println("A idade é: " + Age + " anos.");
+
+        // Tipos Primitivos
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+
+        // Casting: Fazer com que a JVM aceite valores que estejam fora dos limites do tipo primitivo atribuido a variável.
+        double n = 9.75;
+        byte nota = (byte) n;
+
+        int number = 1000;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000.d; // or just: 2000
+        float salarioFloat = 2500.0f; // or just: 2500
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        // Funcional de acordo com a "Tabela ASCII"
+        char caractere = 87;
+
+        System.out.println("A idade é: " + age + " anos.");
+        System.out.println(falso);
+        System.out.println("char: " + caractere);
+        System.out.println("Casting: " + nota);
     }
 }
