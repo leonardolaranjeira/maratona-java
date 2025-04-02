0d020e6e459f32048ec1301576b5eb4cc67a6beb
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-04-01 22:47:22 -0300

    Associação e static
    Associação entre atributos das classes e alguns conceitos sobre modificador estático em blocos de inicialização.

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -4,7 +4,6 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
 
         Anime anime1 = new Anime();
         Anime anime2 = new Anime();
