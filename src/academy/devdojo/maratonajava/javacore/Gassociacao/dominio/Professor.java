32ffb05c255baf8de4846bde8a238e5d38ed9a15
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-04-02 19:19:51 -0300

    Leitura de dados  do teclado
    Método Scanner (exercício pendente)

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -3,7 +3,6 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 public class Professor {
     private String nome;
 
-
     public Professor(String nome) {
         this.nome = nome;
     }
@@ -16,3 +15,18 @@ public class Professor {
         this.nome = nome;
     }
 }
+
+/* Crie um sistema que gerencia seminários
+*
+* O sistema deverá cadastrar seminários, estudantes, professores e local onde será realizado
+*
+* Um aluno poderá estar em apenas um semnário
+* Um seminário poderá ter nenhum ou vários alunos
+* Um professor poderá ministrar vários seminários
+* Um seminário deve ter um local
+*
+* Campos básicos (excluindo relacionamento)
+* seminário: titulo
+* aluno: nome e idade
+* professor: nome e especialidade
+* local: endereço */
