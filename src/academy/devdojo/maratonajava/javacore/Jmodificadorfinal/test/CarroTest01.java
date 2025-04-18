27ca78d1c6dba3ed89626b1383ed263d36915204
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-04-18 18:23:10 -0300

    Sobrescrita, modificador final
    Estudo sobre constantes, modificador "final" em classe, atributos, métodos e também um pouco sobre "toString" (sobrescrita de método).

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Comprador comprador2 = new Comprador();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Voyage");
+        System.out.println(carro.COMPRADOR);
+        // Estudar sobre "singleton pattern"
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("Enzo");
+        ferrari.imprime();
+    }
+}
